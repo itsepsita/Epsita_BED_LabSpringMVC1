6a17418ecc54819615b47f387e502ae894f7be84
itsepsita <77191477+itsepsita@users.noreply.github.com>
2024-04-17 22:30:07 +0530

    Springboot Application class
    Springboot Application class

---
+++ src/main/java/EmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Lab5.EMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMangementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMangementSystemApplication.class, args);
+	}
+
+}
